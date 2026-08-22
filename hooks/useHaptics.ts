226420a41a048d590b09a7b984b7c1b552f305d7
Bt2018 <shengxin.canada@gmail.com>
2026-08-22 20:53:49 +0000

    Remove sample e-comm app

--- hooks/useHaptics.ts
+++
@@ -1,2 +0,0 @@
-import * as Haptics from 'expo-haptics';
-export function useHaptics() { const tap = () => { Haptics.selectionAsync().catch(() => undefined); }; const success = () => { Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success).catch(() => undefined); }; return { tap, success }; }
